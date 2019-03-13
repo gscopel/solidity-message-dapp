3ff2d0b742d4ef7efa8e07cf43a9a9332bea281e
Gary Scopel <Gary@Garys-MacBook-Air.local>
2019-03-13 12:42:08 -0400

    Mocha testing with message and setMessage

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,7 +1,11 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3'); //contructor web3
-const web3 = new Web3(ganache.provider());//instance of web3
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);//instance of web3
+
+const { interface, bytecode } = require('../compile');
 
 //Example of mocha test suite
 
@@ -30,3 +34,36 @@ const web3 = new Web3(ganache.provider());//instance of web3
 //     assert.equal(car.drive(), 'vroom');
 //   })
 // });
+
+let accounts;
+let inbox;
+const INITIAL_STRING = 'Hello';
+const SET_MESSAGE_STRING = 'Bye'
+
+beforeEach(async () => {
+  // Get list of accounts
+  accounts = await web3.eth.getAccounts()
+  //Use one account to deploy contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: [INITIAL_STRING] })
+    .send({ from: accounts[0], gas: '1000000' });
+
+    inbox.setProvider(provider);
+});
+
+describe('Inbox', () => {
+  it('deploys contract', () => {
+    assert.ok(inbox.options.address);
+  });
+
+  it('has default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, INITIAL_STRING);
+  });
+
+  it('can change message', async () => {
+    await inbox.methods.setMessage(SET_MESSAGE_STRING).send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, SET_MESSAGE_STRING);
+  });
+});
